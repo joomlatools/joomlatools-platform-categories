b04505eebcf3fa7078460cae9771f0e97243629e
Johan Janssens <johan@timble.net>
2015-08-18 23:38:20 +0200

    re #3 - Move JCategories and register with the autoloader

--- autoload.php
+++ autoload.php
@@ -7,12 +7,14 @@
  * @link		https://github.com/joomlatools/joomla-platform for the canonical source repository
  */
 
+JLoader::register('JCategories'               , JPATH_ADMINISTRATOR . '/components/com_categories/libraries/categories.php');
 JLoader::register('CategoriesTableCategories' , JPATH_ADMINISTRATOR . '/components/com_categories/tables/categories.php');
 //JLoader::register('CategoriesViewCategories'  , JPATH_SITE . '/components/com_categories/views/categories/view.html.php');
 //JLoader::register('CategoriesViewCategoryfeed', JPATH_SITE . '/components/com_categories/views/category/view.feed.php');
-JLoader::register('JFormFieldCategoryEdit', JPATH_ADMINISTRATOR . '/components/com_categories/models/fields/categoryedit.php');
+
+JLoader::register('JFormFieldCategoryEdit'  , JPATH_ADMINISTRATOR . '/components/com_categories/models/fields/categoryedit.php');
 JLoader::register('JFormFieldCategoryParent', JPATH_ADMINISTRATOR . '/components/com_categories/models/fields/categoryparent.php');
-JLoader::register('JFormFieldCategory', JPATH_ADMINISTRATOR . '/components/com_categories/models/fields/category.php');
+JLoader::register('JFormFieldCategory'      , JPATH_ADMINISTRATOR . '/components/com_categories/models/fields/category.php');
 
 JLoader::registerAlias('JTableCategory'   , 'CategoriesTableCategories');
 JLoader::registerAlias('JViewCategories'  , 'CategoriesViewCategories');
